b4bc764c96148cbad2c3759a12a8479971725af2
TOUTA-T <toutaby@aol.com>
2020-11-17 02:12:42 +0000

    Add CURD page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy, :create]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -13,21 +13,37 @@ class TasksController < ApplicationController
   end
 
   def edit
+
   end
 
   def create
-    # @post = current_user.tasks.build(post_params)
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task), notice:"投稿されました！"
+    else
+      render :new
+    end
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice:"編集は正常に行われました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @task.destroy
+      redirect_to tasks_path, notice:"削除しました"
   end
 
   private
   def set_task
-    @task =Task.find(oarams[:id])
+    @task =Task.find(params[:id])
   end
 
+  def task_params
+    params.require(:task).permit(:name, :details)
+  end
 end
