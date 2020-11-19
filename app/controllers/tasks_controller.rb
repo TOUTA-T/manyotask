8f9307dddf8ad53ca597ebb281732df98aa5d74a
TOUTA-T <toutaby@aol.com>
2020-11-19 11:59:20 +0900

    Fix DESC

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all
-    Task.all.order(created_at: "DESC")
+    @tasks = Task.all.order(created_at: "DESC")
   end
 
   def show
