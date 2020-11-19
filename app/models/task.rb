3c00cceb8074bd1e4aef917a64cbfa2a0b29d3ad
TOUTA-T <toutaby@aol.com>
2020-11-19 11:53:00 +0900

    Fix step2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :detail, presence: true
-  default_scope -> { order(created_at: :desc)}
+  # 並び替えはこちらでもできるが、保守性の観点でモデルのデフォルトにするのは▲
+  # default_scope -> { order(created_at: :desc)}
 end
