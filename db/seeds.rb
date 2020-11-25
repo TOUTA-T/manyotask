d549881b88ccebaf8d21fd7f1f5333c35f0197d2
TOUTA-T <toutaby@aol.com>
2020-11-25 17:55:32 +0900

    Add assosiation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "touta", email:"toutaby@aol.com", password:"password", password_confirmation:"password")
+User.create!(name: "ヤドン", email:"yadon@aol.com", password:"password", password_confirmation:"password")
